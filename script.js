d12fcaad06b74233ca01f33ad1443390fd0d6564
shimayu22 <shimayu22@gmail.com>
2020-05-21 16:22:07 +0900

    ESCを2回押すとN予備校のページになる

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-alert('アイコンがクリックされました。');
+$('.modal-window-inner').css('width', '100%').css('height', '100%').css('left', '0px');
+$('.modal-close').css('display', 'none');
+$('.toggle-sidebar-button').css('display', 'none');
